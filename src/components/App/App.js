6b14930c29dee0152b13b830bc135df013edecab
Vincent Wong <vincentwco2002@gmail.com>
2024-10-31 10:36:55 +0800

    Implement basic structure of Tracklist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 import Searchbar from '../Searchbar/Searchbar'
-import Track from '../Track/Track'
+import Tracklist from '../Tracklist/Tracklist'
+
+const tracks = [{
+  song: 'Complicated',
+  artist: 'Avril Lavigne',
+  album: 'Breakaway'
+  },
+  {
+    song: 'Sk8ter Boi',
+    artist: 'Avril Lavigne',
+    album: 'Breakaway'
+  },
+  {
+    song: 'Bite Me',
+    artist: 'Avril Lavigne',
+    album: 'Love Sux'
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <Searchbar></Searchbar>
-      <Track song='Complicated' artist='Avril Lavigne' album='Breakaway'></Track>
+      <Tracklist tracks={tracks} />
     </div>
   );
 }
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,10 +1,11 @@
+import React from 'react';
 
 function Track(props) {
     return (
         <div className='Track'>
             <div className='Track-info'>
-                <h3>{props.song}</h3>
-                <p>{props.artist} | {props.album}</p>
+                <h3>{props.track.song}</h3>
+                <p>{props.track.artist} | {props.track.album}</p>
             </div>
             <button>+</button>
         </div>
